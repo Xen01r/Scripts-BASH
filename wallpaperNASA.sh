9b531b113fe0aa3ef3e48d473e387f42de8df8fe
clement <clement.eustache1@gmail.com>
2019-04-04 18:33:57 +0200

    Script retrieving the daily TERRA Modis (NASA) satellite image

---
+++ wallpaperNASA.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+DATE=$(date "+%Y-%m-%d")
+TS=$(echo $(($(date +%s%N)/1000000)))
+
+cd /tmp/
+
+/usr/bin/wget -t 1 'https://wvs.earthdata.nasa.gov/api/v1/snapshot?REQUEST=GetSnapshot&LAYERS=MODIS_Terra_CorrectedReflectance_TrueColor&CRS=EPSG:4326&TIME='${DATE}'&BBOX=41.23,-11.71,51.91,15.905&FORMAT=image/jpeg&WIDTH=6284&HEIGHT=2430&AUTOSCALE=TRUE&ts='${TS}'&DOWNLOAD=yes' -O tmp.jpg	#Download image
+
+if [ $(echo "100 * $(/usr/bin/identify -verbose tmp.jpg | grep 'Image statistics' -A 8 | grep 'entropy' | cut -d ':' -f 2) > 95" | bc) ]
+then
+    /usr/bin/convert /tmp/tmp.jpg -resize 1920 /tmp/tmp_resize.jpg
+    /usr/bin/convert /tmp/tmp_resize.jpg -crop 1920x1080+0+0 /tmp/wallpaper.jpg
+fi
+
+rm /tmp/tmp.jpg /tmp/tmp_resize.jpg
+mv /tmp/wallpaper.jpg $HOME/Images/
